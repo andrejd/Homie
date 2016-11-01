b8e5ef7c4fa869c1248641d4ba6c9c21509a37f8
Andrej Debeljak <andrejdebeljak@gmail.com>
2016-11-01 10:23:10 +0100

    Preferences completed

--- app/src/main/java/com/kvajpoj/homie/activities/SettingsActivity.java
+++ app/src/main/java/com/kvajpoj/homie/activities/SettingsActivity.java
@@ -16,8 +16,6 @@ import android.widget.BaseAdapter;
 import com.kvajpoj.homie.R;
 
 import org.apache.log4j.Logger;
-import org.apache.log4j.varia.StringMatchFilter;
-
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -51,29 +49,16 @@ public class SettingsActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    //@Override
-    //protected void onPause() {
-
-    //    super.onPause();
-    //}
-
     public static class SettingsFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener {
 
-        //Preference mRefreshOnBackgroundPreference;
-        //Preference mVersionPreference;
-        //String mBuildTime = "";
         EditTextPreference mServerAddress;
         EditTextPreference mUsername;
         EditTextPreference mPassword;
         EditTextPreference mServerPort;
 
-        //public void setBuildTime(String buildTime) {
-        //    mBuildTime = buildTime;
-        //}
-
         private Logger LOG;
         private SharedPreferences sharedPref;
-
+        private String MqttDefaultPort = "1883";
 
         @Override
         public void onCreate(Bundle savedInstanceState) {
@@ -87,15 +72,21 @@ public class SettingsActivity extends AppCompatActivity {
 
             mServerAddress = (EditTextPreference) findPreference("pMqttServerAddress");
             mServerAddress.setOnPreferenceChangeListener(this);
-            mServerAddress.setSummary(sharedPref.getString("pMqttServerAddress","Tap to set Mqtt server address"));
+            String address = sharedPref.getString("pMqttServerAddress", getString(R.string.prefServerPrompt));
+            if (address.isEmpty()) address = getString(R.string.prefServerPrompt);
+            mServerAddress.setSummary(address);
 
             mServerPort = (EditTextPreference) findPreference("pMqttServerPort");
             mServerPort.setOnPreferenceChangeListener(this);
-            mServerPort.setSummary(sharedPref.getString("pMqttServerPort","1833"));
+            String port = sharedPref.getString("pMqttServerPort", MqttDefaultPort);
+            if(port.isEmpty()) port = MqttDefaultPort;
+            mServerPort.setSummary(port);
 
             mUsername = (EditTextPreference) findPreference("pMqttUsername");
             mUsername.setOnPreferenceChangeListener(this);
-            mUsername.setSummary(sharedPref.getString("pMqttUsername","Tap to set username"));
+            String username = sharedPref.getString("pMqttUsername", getString(R.string.prefUsernamePrompt));
+            if(username.isEmpty()) username = getString(R.string.prefUsernamePrompt);
+            mUsername.setSummary(username);
 
             mPassword = (EditTextPreference) findPreference("pMqttPassword");
             mPassword.setOnPreferenceChangeListener(this);
@@ -107,31 +98,34 @@ public class SettingsActivity extends AppCompatActivity {
 
         @Override
         public boolean onPreferenceChange(Preference preference, Object newValue) {
-            if (LOG.isDebugEnabled()) LOG.debug("preference changed " + preference.getKey());
+
+            if ( preference.getKey().equals("pMqttServerAddress") ){
+                String server = (String) newValue;
+                if(server.isEmpty()) server = getString(R.string.prefServerPrompt);
+                preference.setSummary(server);
+            }
+
+            if ( preference.getKey().equals("pMqttServerPort") ){
+                String port = (String) newValue;
+                if(port.isEmpty()) port = MqttDefaultPort;
+                preference.setSummary(port);
+            }
 
             if ( preference.getKey().equals("pMqttPassword") ){
-                String password = (String) newValue;//sharedPref.getString("pMqttPassword", getString(R.string.prefPasswordPrompt));
-                LOG.debug(password);
-                if(password.isEmpty()) {
-                    password = getString(R.string.prefPasswordPrompt);
-                }
-                else {
-                    password = password.replaceAll("(?s).", "*");
-                }
-
-                LOG.debug("Setting summary to " + password);
+                String password = (String) newValue;
+                if(password.isEmpty()) password = getString(R.string.prefPasswordPrompt);
+                else password = password.replaceAll("(?s).", "*");
                 preference.setSummary(password);
-                //mPassword.setSummary(password);
+            }
 
+            if ( preference.getKey().equals("pMqttUsername") ){
+                String username = (String) newValue;
+                if(username.isEmpty()) username = getString(R.string.prefUsernamePrompt);
+                preference.setSummary(username);
             }
 
             ((BaseAdapter)getPreferenceScreen().getRootAdapter()).notifyDataSetChanged();
 
-
-
-
-
-            //preference.setSummary(sharedPref.getString(preference.getKey(), (String) preference.getSummary()));
             return true;
         }
 
